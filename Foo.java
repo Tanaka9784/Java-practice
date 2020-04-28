d3e2917aa97351d679eff9d009b70191472578de
sakura <sakura1@example.com>
2020-04-28 18:16:52 +0100

    insert System.gc()

--- Foo.java
+++ Foo.java
@@ -4,7 +4,12 @@
  *
  * **/
 public class Foo<T>{
+	Foo(){
+	}
 	public static void main(String[] args){
 		Foo<int[]> foo = new Foo<>();	
+		int[] a = {1,2,3};
+		Class<int[]> klass = int[].class;
+		System.gc();
 	}
 }
