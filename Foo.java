779b7425d4500b008fe64d545581c02e0185265d
sakura <sakura1@example.com>
2020-04-29 18:18:54 +0100

    add a type parameter

--- Foo.java
+++ Foo.java
@@ -3,13 +3,22 @@
  *
  *
  * **/
-public class Foo<T>{
-	Foo(){
+public class Foo{
+	public Foo(){
 	}
 	public static void main(String[] args){
-		Foo<int[]> foo = new Foo<>();	
 		int[] a = {1,2,3};
 		Class<int[]> klass = int[].class;
 		System.gc();
+		Bar<? extends Object, ? extends Object> bar = new Foo().new Bar<Object, Object>();
+		bar.doIt(bar);
+	}
+	public class Bar<T, U extends T>{
+		public Bar(){
+		}
+		public <TT> void doIt(TT tt){
+		}
+		public void doIt(Bar<?,?> bar){
+		}
 	}
 }
