7ea7b4d731089256f31ae706fb275e02bf005940
sakura <sakura1@example.com>
2020-05-03 18:48:47 +0100

    add the method myprint()

--- Foo.java
+++ Foo.java
@@ -28,24 +28,44 @@ public class Foo{
 			//ArrayList<Object> objlist320 = objlist32;
 			//out.println(Arrays.asList(new Object(), new Object(), new Object()));
 		//Arrays.toString()
-		int[] intarray35 = new int[]{0,1,2}; out.println(Arrays.toString(intarray35));
+		int[] intarray35 = new int[]{0,1,2}; out.print("35: " + Arrays.toString(intarray35) + ", ");
 		Object[] objarray35 = {new Object(), new Object()}; out.println(Arrays.toString(objarray35));
 		//inference variable T has incompatible bounds
 		List<int[]> intlist440 = Arrays.asList(new int[]{1,2,3});
 			//List<Integer> intlist440 = Arrays.asList(new int[]{1,2,3});
+		//Collection.toArray()
+		out.println("35: " + Arrays.toString(new ArrayDeque<Integer>(Arrays.asList(0,1)).toArray()));
 		//reference types and primitive types
-		// 1) A array doesn't reference a primitive data.
+		// 1) A array doesn't reference a primitive data and a reference type.
 		int i360 = 0; int i361 = 1; int i362 = 2;
 		int[] intarray39 = new int[]{i360, i361, i362}; i360 = 3;
-		out.println(Arrays.toString(intarray39));
+		out.print("36: " + Arrays.toString(intarray39) + ", "); 
+		String s360 ="s360"; String[] sa36 = new String[]{s360}; s360 = "s361";
+		out.println(Arrays.toString(sa36));
 		// 2) List<T> doesn't have add() 
 		List<Integer> il44 = new LinkedList<Integer>(Arrays.asList(0,1,2)); il44.add(3);
 		// 3) List<T> doesn't reference a reference type.
 		String s45 = "abc";
 		List<String> ls45 = new ArrayList<>(); 
-		ls45.add(s45); out.println(ls45);
-		s45 = "bcd"; out.println(ls45);
-		ls45.set(0,"cde"); out.println(ls45); out.println(s45);
+		ls45.add(s45); myprint(45,ls45);
+		s45 = "bcd"; myprint(ls45);
+		ls45.set(0,"cde"); myprint(ls45); myprint(0,s45);
+		//ArrayDeque, Stack, FIFO, FIFO
+		Deque<Object> do52 = new ArrayDeque<Object>(Arrays.asList(new Object(),new Object()));
+		do52.addFirst(new Object()); do52.getFirst();
+		Stack<Object> so52 = new Stack<Object>();
+		so52.push(new Object()); so52.pop();
+	}
+	//enhanced out.print for me
+	public static void myprint(Object obj){
+		out.print(obj + ", ");
+	}
+	public static void myprint(int ln, Object obj){
+		if(ln == 0){
+			out.println(obj);
+		}else{
+			out.print(ln + ": " + obj + ", ");
+		}
 	}
 	//generic class
 	public class Bar<T, U extends T>{
